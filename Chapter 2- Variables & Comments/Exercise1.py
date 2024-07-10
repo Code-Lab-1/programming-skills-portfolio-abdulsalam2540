91014b46a0d51e3ec4c930f9c62b55c306d41884
Abdul salam <115148201+abdulsalam2540@users.noreply.github.com>
2024-07-10 17:05:32 +0400

    Add files via upload

---
+++ Chapter 2- Variables & Comments/Exercise1.py
@@ -0,0 +1,9 @@
+# Assign the initial message to a variable
+communication = "Wellcome to the , world!"
+# Print the initial message
+print(communication)
+
+# Change the value of the variable to a new message
+communication = "Goodbye to the, world!"
+# Print the new message
+print(communication)
---
+++ Chapter 2- Variables & Comments/Exercise2.py
@@ -0,0 +1,6 @@
+# Assign the quote and the author to variables
+recite = "The only limit to our realization of tomorrow is our doubts of today."
+writer = "Franklin D. Roosevelt"
+
+# Print the quote and the author's name in the desired format
+print(f'{writer} once said, "{recite}"')
